5f457ff48809781229403e2340ccd93c1431a11a
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-12 12:24:04 +0700

    upd

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    products: {
+        1: 'https://trnsfx.ru/go/s3wq40bpow',
+        2: 'https://trnsfx.ru/go/s0uusze41d',
+        3: 'https://trnsfx.ru/go/sh5ztmjczt',
+        4: 'https://trnsfx.ru/go/s2s0j4ozv5'
+    }
+};
